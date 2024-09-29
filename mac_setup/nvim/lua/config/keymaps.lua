2aa7f7a88fc25368267c9a4307b42bb1ed55ac52
duytechie <trand2221@gmail.com>
2024-09-29 20:41:17 +0700

    Add mac setup

---
+++ mac_setup/nvim/lua/config/keymaps.lua
@@ -0,0 +1,11 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>p", '<cmd>lua require("cmp").setup { enabled = true }<cr>', { desc = "Enable completion" })
+vim.keymap.set(
+  "n",
+  "<leader>P",
+  '<cmd>lua require("cmp").setup { enabled = false }<cr>',
+  { desc = "Disable completion" }
+)
